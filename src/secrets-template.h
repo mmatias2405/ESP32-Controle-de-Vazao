139b1f02ebb3db055ceb0383b7132f21962866b9
Matheus Matias <matheusmatias555@gmail.com>
2026-02-28 18:25:51 -0300

    Commit Inicial

---
+++ src/secrets-template.h
@@ -0,0 +1,11 @@
+// Renomeie este arquivo para secrets.h e preencha seus dados
+
+//informações do Wi-FI
+#define SECRET_SSID "COLOQUE_SEU_WIFI_AQUI"
+#define SECRET_PASS "COLOQUE_SUA_SENHA_AQUI"
+
+//informações do Serivodor MQTT
+#define MQTT_SERVER "COLOQUE_O_IP_DO_SERVIDOR_AQUI";
+#define MQTT_PORT "COLOQUE_A_PORTA_DO_MQTT_AQUI";
+#define MQTT_USER "COLOQUE_SEU_USUARIO_AQUI;
+#define MQTT_PASS "COLOQUE_SUA_SENHA_AQUI";
